d9acc01346eabe163abcc2f3bd6b13d0a6e57633
Miguel-Lies <miguelmendes2031@gmail.com>
2026-05-18 12:46:16 -0300

    feat: repositório de roles

---
+++ API-bank-reconfiguration/repository/RolesRepository.java
@@ -0,0 +1,7 @@
+package com.enterprise.bank_lies.repository;
+
+import com.enterprise.bank_lies.entity.Roles;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolesRepository extends JpaRepository<Roles, Integer> {
+}
